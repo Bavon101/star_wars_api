88e9c827af205b37f04b42d2c5485bb66807617a
Bavon101 <bavonfate@gmail.com>
2022-05-09 08:53:12 +0300

    Add express and call app with API routes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const {search} = require("./lib/search");
+const {people} = require("./lib/people");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get("/people", people);
+app.get("/search", search);
+exports.app = functions.https.onRequest(app);
+
+
